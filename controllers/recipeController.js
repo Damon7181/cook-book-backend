2d647783f2b95d8e33148e24da4552c9ad6e639f
Ubaid ur Rehman <damon7181@gmail.com>
2025-07-17 13:54:15 +0500

    Adding author name in Recipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -138,7 +138,7 @@ async function createRecipe(req, res) {
       cuisine: recipeData.cuisine,
       image: recipeData.image,
       cookingTime: recipeData.cookingTime,
-      authorId: req.user,
+      author: req.user.userName,
       authorId: req.user.userId,
       ingredients: { create: recipeData.ingredients },
       instructions: { create: recipeData.instructions },
